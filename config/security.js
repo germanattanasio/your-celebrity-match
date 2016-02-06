7fb3722baf4a0826a878e78542701676ea593638
German Attanasio Ruiz <germanattanasio@gmail.com>
2016-02-06 16:44:03 -0500

    added rate limitation

--- config/security.js
+++ config/security.js
@@ -34,12 +34,12 @@ module.exports = function (app) {
   var limiter = rateLimit({
     windowMs: 30 * 1000, // seconds
     delayMs: 0,
-    max: 3,
+    max: 10,
     message: JSON.stringify({
       error:'Too many requests, please try again in 30 seconds.',
       code: 429
     }),
   });
 
-  app.use('/api/', limiter);
+  app.use('/like/', limiter);
 };
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export VCAP_SERVICES=`cat VCAP_SERVICES.json`
-
-nodemon app.js
